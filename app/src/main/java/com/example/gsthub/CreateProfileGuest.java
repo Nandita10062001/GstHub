a87ff79b4bfe8f384da562ff32ca07d7f3648d68
Nipun Manghi <nipunm59@gmail.com>
2020-10-05 23:08:03 +0530

    1st commit

---
+++ app/src/main/java/com/example/gsthub/CreateProfileGuest.java
@@ -0,0 +1,14 @@
+package com.example.gsthub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateProfileGuest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile_guest);
+    }
+}
