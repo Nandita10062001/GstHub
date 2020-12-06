4cc9d731820ce7d585f5e00f6b61ed5454975dd4
pournami09 <pournami.pottekat@gmail.com>
2020-12-06 06:30:48 +0530

    10th commit

--- app/src/main/java/com/example/gsthub/MainActivity2.java
+++ app/src/main/java/com/example/gsthub/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +18,7 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         signInPageOneBtn=findViewById(R.id.signInPageOneBtn);
         registerPageOneBtn=findViewById(R.id.registerPageOneBtn);
+
 
         signInPageOneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
